a46caf7e17ae8c118d9111991b6a4cdf17ba37a8
yateskha000 <yateskha000@warren.k12.in.us>
2021-11-05 11:41:50 -0400

    U2-Project

--- RPGCharacterGUI/src/RPGCharacterGUI.java
+++ RPGCharacterGUI/src/RPGCharacterGUI.java
@@ -1,2 +1,108 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+
+
 public class RPGCharacterGUI {
+
+    static JTextField input, input2, input3, input4;
+    static JLabel results;
+    Color lightGray= new Color(115,115,115);
+
+    public RPGCharacterGUI() {
+        JFrame window= new JFrame("RPG Ting");
+        JPanel panel = new JPanel();
+        input=new JTextField(15);
+        input2=new JTextField(15);
+        input3=new JTextField(15);
+        input4=new JTextField(15);
+
+        JLabel inputLabel=new JLabel("Enter Your  Strength level: ");
+        JLabel inputLabel2=new JLabel("Enter Your Intelligence Level: ");
+        JLabel inputLabel3=new JLabel("Enter Your Leadership Level: ");
+        JLabel inputLabel4=new JLabel("Enter Your Karma Level: ");
+
+        JButton button = new JButton("Calculate Final Battle Power");
+
+
+        results= new JLabel("00.0");
+
+        window.setSize(200, 400);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+
+        button.addActionListener(new ButtonListener());
+
+
+        button.setBackground(Color.CYAN);
+        panel.setBackground(lightGray);
+        panel.add(inputLabel);
+        panel.add(input);
+        panel.add(inputLabel2);
+        panel.add(input2);
+        panel.add(inputLabel3);
+        panel.add(input3);
+        panel.add(inputLabel4);
+        panel.add(input4);
+
+        panel.add(button);
+        panel.add(results);
+        window.add(panel);
+        window.setVisible(true);
+
+    }
+
+
+
+    private static class ButtonListener implements ActionListener{
+        DecimalFormat round= new DecimalFormat("##");
+        public void actionPerformed(ActionEvent e) {
+          double strength=Double.parseDouble(input.getText());
+          double Intell=Double.parseDouble(input2.getText());
+          double Leader=Double.parseDouble(input3.getText());
+          double Karma=Double.parseDouble(input4.getText());
+             results.setText(String.valueOf(round.format(strength*Intell/Leader+Karma)));
+
+
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- WidgetFactory/src/WidgetFactory.java
+++ WidgetFactory/src/WidgetFactory.java
@@ -1,31 +1,97 @@
+import javax.swing.*;
+import java.text.DecimalFormat;
+
+
 public class WidgetFactory {
+static  double widPayday=16.50;
+static int widPay=10;
+static int workers=5;
+static int shift=16;
 
-    /*
-    Lessons L1 - L3
+    public static void main(String[] args) {
+        int widNum=Integer.parseInt(JOptionPane.showInputDialog("How many Widgets did you want to make out of 10's?"));
+        DecimalFormat round= new DecimalFormat("##");
 
-    You have been asked by the manager of the Widget Factory to create a program which will tell how many days it will take to produce a number of widgets.
-    It should also output the cost of the widgets, the cost to produce the widget and the profit.
 
-    Specific Details:
-     - 10 Widgets are made each hour
-     - Plant runs 2 shifts of 8 hours a day, requiring 5 workers a shift
-     - each worker makes $16.50 an hour
-     - each widget sells for $10
 
-     Programming Details:
-     - prompt the user for the number of widgets using pop-up boxes
-     - display # of widgets, # of days, Cost of production, Cost of widgets and profit
-     - 2 or more (preferably more) methods - at least 1 to calculate # of day and one to calculate production costs
-     - only WHOLE number of days, no half or partial days.
-     - Output should be in proper currency form
+        widgetDay(widNum,widPay);
+        laborProfit(widNum);
+        WidgetProfit(widNum);
+        totalProfit(widNum);
 
-     Example Run:
+    JOptionPane.showMessageDialog(null,"To make : "+widNum+" Widgets it would take "+round.format(+widgetDay(widNum,widPay))+" days, making  $"+WidgetProfit( widNum)+" in production profit, costing  $" +
+            laborProfit(widNum)+  " but making the total profit of $" +totalProfit(widNum)+"");
 
-     Number of Widgets: 1000
-     # of Days: 7
-     Cost of Widgets: $10,000.00
-     Cost of Production: $9,240.00
-     Profit: $760.00
 
-     */
+
+        System.exit(0);
+
+
+    }
+    public static double WidgetProfit(int widNum){
+
+        return widNum*10;
+
+
+
+    }
+
+    public static double widgetDay(int widNum, int widPay){
+
+        double dayTime=widPay*16;
+
+
+        return widNum/dayTime+1;
+
+
+    }
+    public static double laborProfit(int widNum){
+
+        double dayTime=widPay*16;
+        double dayNeed=widNum/dayTime+1;
+        double hoursNeed=dayNeed*shift;
+
+   double totalPay=workers*widPayday;
+
+        return hoursNeed*totalPay;
+
+
+
+    }
+    public static double totalProfit(int widNum){
+        int widProf=widNum*10;
+        double dayTime=widPay*16;
+        double dayNeed=widNum/dayTime+1;
+        double hoursNeed=dayNeed*shift;
+
+        double totalPay=workers*widPayday;
+        double laborProfit=hoursNeed*totalPay;
+
+        return widProf-laborProfit;
+    }
 }
+ /* You have been asked by the manager of the Widget Factory to create a program which will tell how many days it will take to produce a number of widgets.
+    It should also output the cost of the widgets, the cost to produce the widget and the profit.
+Lessons L1 - L3
+    Specific Details:
+            - 10 Widgets are made each hour
+        - Plant runs 2 shifts of 8 hours a day, requiring 5 workers a shift
+        - each worker makes $16.50 an hour
+        - each widget sells for $10
+
+    Programming Details:
+            - prompt the user for the number of widgets using pop-up boxes
+        - display # of widgets, # of days, Cost of production, Cost of widgets and profit
+        - 2 or more (preferably more) methods - at least 1 to calculate # of day and one to calculate production costs
+        - only WHOLE number of days, no half or partial days.
+        - Output should be in proper currency form
+
+    Example Run:
+
+    Number of Widgets: 1000
+            # of Days: 7
+    Cost of Widgets: $10,000.00
+    Cost of Production: $9,240.00
+    Profit: $760.00
+
+            */
